35e7aa6de1caa8bf77475b9281cf4348fd956b95
dbellido <becadiego@gmail.com>
2016-07-26 16:57:31 +0200

    ALS_OSRAM

---
+++ ALS_OSRAM/pointclouds/ALS_OSRAM/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 43411,
+    "boundingBox": {
+        "lx": -2049.40625,
+        "ly": -1247.122,
+        "lz": -45.731,
+        "ux": 2173.0509999999995,
+        "uy": 2975.335249999999,
+        "uz": 4176.72625
+    },
+    "tightBoundingBox": {
+        "lx": -6174.803249724535,
+        "ly": -5790.121999999999,
+        "lz": 369.0482500147233,
+        "ux": -1955.0435007044825,
+        "uy": -2822.022250742025,
+        "uz": 432.2924999865681
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 36.56755065917969,
+    "scale": 0.01,
+    "hierarchyStepSize": 5
+}
